e6d1f2c7c2c6f1ee0fcb6b862564bf18337d63d2
siddartha31-bit <siddartha3101@gmail.com>
2026-05-29 10:17:54 +0530

    Add: permutation-string - submission-0

---
+++ Data Structures & Algorithms/permutation-string/submission-0.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    bool checkInclusion(string s1, string s2) {
+        if (s1.size() > s2.size()) return false;
+        
+        vector<int> count1(26, 0), count2(26, 0);
+        
+        for (int i = 0; i < s1.size(); i++) {
+            count1[s1[i] - 'a']++;
+            count2[s2[i] - 'a']++;
+        }
+        
+        if (count1 == count2) return true;
+        
+        for (int i = s1.size(); i < s2.size(); i++) {
+            count2[s2[i] - 'a']++;
+            count2[s2[i - s1.size()] - 'a']--;
+            
+            if (count1 == count2) return true;
+        }
+        
+        return false;
+    }
+};
